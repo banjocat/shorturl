114d1028d56215f15cd2fac9535e7d3b12334e36
banjocat <jackmuratore@gmail.com>
2019-06-30 23:55:47 -0400

    let wesley crash

--- wesley/app.js
+++ wesley/app.js
@@ -12,10 +12,6 @@ client.on('connect', function() {
     console.log('Redis client connected');
 });
 
-client.on('error', function (err) {
-    console.log('Something went wrong ' + err);
-});
-
 http.createServer(function(req, res) {
   let path = req.url.slice(1); // remove leading /
   client.get(path, function(err, data) {
